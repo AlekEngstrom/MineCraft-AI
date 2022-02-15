ffad4849ea281c4ca2c66491384bc19487f5d561
Alek Engstrom <72417360+AlekEngstrom@users.noreply.github.com>
2022-02-14 23:42:41 -0700

    Speed up

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
+
+from collections import deque
 from copy import deepcopy
 from dataclasses import make_dataclass
 from distutils.archive_util import make_archive
+from os import path
 from re import T
 from telnetlib import X3PAD
 from tkinter import E
+from traceback import print_tb
 from pyautogui import *
 import pyautogui
 import time
@@ -26,7 +30,7 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     time.sleep(0.1) #This pauses the script for 0.1 seconds
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    print("die")
+    1("die")
 def eat(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
@@ -58,7 +62,7 @@ def getDirection():
     Direction = cleanImg(Direction)
 
     direction = pytesseract.image_to_string(Direction)
-    print(direction)
+
     open = 0
     slash = 0
     close = 0
@@ -86,7 +90,6 @@ def getCoords():
     Coords = cleanImg(Coords)
     coords = pytesseract.image_to_string(Coords)
     copy = deepcopy(coords)
-    print(coords)
     xyz = []
     try:
         for _ in range(3):
@@ -121,21 +124,42 @@ def calibrate(I, J):
             if(failC > 3):
                 mouse.move(100, 100)
                 failC = 0
+                return
         else:
             if(I == 180):
                 if(I < 0):
                     I = 360 - abs(I)
                 if(i < 0):
                     i = 360 - abs(i)
-
-            moveAmount = 100
+            moveAmount = 50
             if(abs(I-i) > 10):
-                moveAmount = 1000
+                moveAmount = 100
             if(abs(I-i) > 20):
-                moveAmount = 3000
-            if(abs(I-i) > 90):
-                moveAmount = 5000
+                mouse.move(100, 0)
+                i2, j2 = getDirection()
+                speed = abs(abs(i)-abs(i2))
+                speed = 3
+                for _ in range(int(abs(I-i)/speed)):
+                    time.sleep(0.01)
+                    if i < I-1:
+                        mouse.move(100, 0)
+                    elif i > I+1:
+                        mouse.move(-100, 0)
+                continue
+            if(abs(J-j) > 20):
+                mouse.move(100, 0)
+                i2, j2 = getDirection()
+                speed = abs(abs(j)-abs(j2))
+                speed = 3
 
+                for _ in range(int(abs(J-j)/speed)):
+                    time.sleep(0.01)
+                    if j < J-2:
+                        mouse.move(0, 100)
+                    elif j > J+2:
+                        mouse.move(0, -100)
+                continue
+                
             if(abs(i) <= 360) & (abs(j) <= 180):
                 if i < I-1:
                     mouse.move(moveAmount, 0)
@@ -148,7 +172,6 @@ def calibrate(I, J):
                         mouse.move(0, -100)
                     else:
                         calibrated = True
-        
 
     time.sleep(1)
 def centerOnBlock():
@@ -187,7 +210,6 @@ def centerOnBlock():
         else:
             print("faild block")
     keyboard.release('alt')
-    print(xyz)
 def buildPlatform(x,y,platform):
     centerOnBlock()
     time.sleep(1)
@@ -274,16 +296,115 @@ def buildPlatform(x,y,platform):
     keyboard.release('alt')
 
     centerOnBlock()
+
+#____________________________________________________________________________________________________________________________________________
+def buildShape(path, platform):
+    x = 0
+    y = 0
+    for i in range(len(path)):
+        if(path[i].y == y) & (path[i].x == x):
+            continue
+        if(path[i].x > x):
+            calibrate(90,80)
+        if(path[i].x < x):
+            calibrate(-90,80)
+        if(path[i].y > y):
+            calibrate(180,80)
+        if(path[i].y < y):
+            calibrate(0,80)
+        
+        keyboard.press('alt')
+        #time.sleep(.1)
+        keyboard.press('s')
+        time.sleep(.7)
+        keyboard.release('s')
+        keyboard.press(platform[path[i].x][path[i].y])
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
+
+
+        keyboard.release(platform[path[i].x][path[i].y])
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)
+        
+        win32api.mouse_event(win32con.MOUSEEVENTF_MIDDLEDOWN,0,0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MIDDLEUP,0,0)
+
+        time.sleep(.3)
+        keyboard.release('alt')
+
+        x = path[i].x
+        y = path[i].y
+        
+class Node:
+    # (x, y) represents coordinates of a cell in the matrix
+    # maintain a parent node for the printing path
+    def __init__(self, x, y, parent=None):
+        self.x = x
+        self.y = y
+        self.parent = parent
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    def get(self):
+        return self.x, self.y
+def getPath(node, path=[]):
+    if node:
+        getPath(node.parent, path)
+        path.append(node)
+def findPath(grid, x=0, y=0):
+    
+    row = [-1, 0, 0, 1]
+    col = [0, -1, 1, 0]
+    path = []
+    q = deque()
+    src = Node(x,y)
+    q.append(src)
+    
+    visited = set()
+    key = (src.x, src.y)
+    visited.add(key)
+    N = len(grid)
+    while q:
+        current = q.popleft()
+        i = current.x
+        j = current.y
+        
+        
+        if(grid[i][j] != "1"):
+            
+            path = []
+            getPath(current, path)
+            print(path)
+
+            grid[i][j] = "1"
+            path += findPath(grid = grid,x = i, y = j)
+            return path
+        
+
+        for k in range(len(row)):
+            x = i + row[k]
+            y = j + col[k] 
+            if(x < N) & (y < N) & (x >= 0) & (y >= 0):
+                next = Node(x, y, current)
+                key = (next.x, next.y)
+                if key not in visited:
+                    q.append(next)
+                    visited.add(key)
+    return []
+                    
+                    
 def makeCircle(width, height, a, b, r):
     p = [[]]
     p = [['1' for x in range(width)] for y in range(height)]
-    EPSILON = 2.2
+    EPSILON = 4
     # draw the circle
     for y in range(height):
         for x in range(width):
             # see if we're close to (x-a)**2 + (y-b)**2 == r**2
             if abs((x-a)**2 + (y-b)**2 - r**2) < EPSILON**2:
-                p[y][x] = '2'
+                p[y][x] = '#'
 
     for line in p:
         print (' '.join(line))
@@ -311,7 +432,8 @@ def makeSphere(size, radius):
                         deb>=0: inside the sphere.
                         deb<0: outside the sphere.'''   
                 deb = radius - ((x0-x)**2 + (y0-y)**2 + (z0-z)**2)**0.5
-                if (deb)>=0: AA[x][y][z] = "2"
+                if (deb>=0) & (deb < 1): 
+                    AA[x][y][z] = "2"
     return AA
 def makeTorus(size, radius, r2):
 
@@ -324,36 +446,71 @@ def makeTorus(size, radius, r2):
 
     for x in range(x0-radius, x0+radius+1):
         for y in range(y0-radius, y0+radius+1):
-            for z in range(z0-radius, z0+radius+1):
- 
-                deb = (r2 - math.sqrt(((x)**2 + (y)**2)))**2 + (z)**2 - radius**2
-                if ((deb) <= 0): AA[x][y][z] = "#"
+            for z in range(z0-radius, z0+radius+1): 
+                deb = (r2 - math.sqrt(((x0-x)**2 + (y0-y)**2)))**2 + (z0-z)**2 - radius**2
+                if (deb>=0) & (deb < 4): 
+                    AA[x][y][z] = "2"
     return AA  
 
-#time.sleep(3)
+
+
+
+to = time.time()
 np.set_printoptions(threshold=sys.maxsize)
-
-R3 = makeSphere(11, 5)
-
-for i in R3:
-    for j in i:
-        print(j)
-    print("+++++++++++++++")
-
-#centerOnBlock()
-#buildPlatform(11,11,platform = p)
-time.sleep(5)
-
+R3 = makeSphere(33,15)
+#R3 = makeTorus(25,5,10)
+path = []
+for i in R3[8]:
+    #for line in i:
+    print (' '.join(i))
+    print("__________________________________________")
+print(findPath(R3[8]))
+'''
+time.sleep(15)
 for i in R3:
 
-    buildPlatform(15,15, i)
+    x = 0
+    y = 0
+    try:
+        if(path != []):
+            x = path[-1].x
+            y = path[-1].y
+            print(x,y, " is the last item in the last path +++++++++++++++++++++++++")
+    except:
+        print("sorry")
+    
+    calibrate(90,90)
+    keyboard.press(i[x][y])
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
+    keyboard.press('space')
+    time.sleep(0.5)
+    keyboard.release('space')
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)
+    keyboard.release(i[x][y])
+    layer = i
+    c1 = deepcopy(layer)
+    #parallel 
+    path = findPath(c1,x,y+1)
+    print(path)
+    print("+++++++++++++++++++++++++++++++++++++++++++++")
+    centerOnBlock()
+    buildShape(path, layer)
+
+#print("The path is : ", path)
+print("This took: ", time.time() - to, " seconds")
+
+'''
+'''
+for i in R3:
+
+    buildPlatform(11,11, i)
     calibrate(90,80)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
     keyboard.press('space')
     time.sleep(0.5)
     keyboard.release('space')
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)
-
+'''
 '''
 keyboard.press('alt')
 keyboard.press('s')
